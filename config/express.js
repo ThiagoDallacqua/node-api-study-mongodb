903af3320753713d7cc3b19a5d2dd439238a7f2b
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-09-15 01:56:17 -0300

    fix cors

--- config/express.js
+++ config/express.js
@@ -18,12 +18,12 @@ module.exports = function() {
   app.use(bodyParser.json());
   app.use(expressValidator());
 
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://payment-app.surge.sh/');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+  // app.use(function(req, res, next) {
+    //   res.header('Access-Control-Allow-Origin', 'http://payment-app.surge.sh/');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //   next();
+  // });
 
   consign()
   .include('controllers')
--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,11 +1,17 @@
 var logger = require('../servicos/logger.js')
+var cors = require('cors');
 
 const keySecret = process.env.STRIPE_SECRET_KEY;
 
 const stripe = require("stripe")(keySecret);
 
+const corsOptions = {
+  origin: 'http://payment-app.surge.sh',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 module.exports = app => {
-  app.post("/stripe/charge", (req, res) => {
+  app.post("/stripe/charge", cors(), (req, res) => {
   let amount = 100;
 
 
